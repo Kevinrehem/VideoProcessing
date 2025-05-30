0c118a8d11472d282f13534234ba84baccedb92f
Kevin Rehem <kevin.rehem@gmail.com>
2025-05-30 18:35:22 -0300

    (fix) Decomposição do código
    Código foi decomposto em métodos menores realizando funções específicas do código para melhorar a legibilidade e manutenção do código

--- src/main/java/SaltPepperCleaner.java
+++ src/main/java/SaltPepperCleaner.java
@@ -8,34 +8,49 @@ public class SaltPepperCleaner extends Thread{
     private static List<byte[][]> fixedFrames = new ArrayList<>();
     private byte[][] currentFrame;
 
+    private List<Byte> getNeighbours(byte[][] currentFrame, int i, int j){
+        List<Byte> pixels = new ArrayList<>();
+        if(currentFrame[i-1] != null){
+            pixels.add(currentFrame[i-1][j]);
+            if(j-1>=0){
+                pixels.add(currentFrame[i-1][j-1]);
+                pixels.add(currentFrame[i][j-1]);
+            }
+            if(j+1<currentFrame[i].length){
+                pixels.add(currentFrame[i-1][j+1]);
+                pixels.add(currentFrame[i][j+1]);
+            }
+        }
+        if(currentFrame[i+1] != null){
+            pixels.add(currentFrame[i+1][j]);
+            if(j-1>=0) pixels.add(currentFrame[i+1][j-1]);
+            if(j+1<currentFrame[i].length) pixels.add(currentFrame[i-1][j+1]);
+        }
+        Collections.sort(pixels);
+
+        return pixels;
+    }
+
+    private byte calcMediana(List<Byte> pixels){
+        byte mediana;
+        int meio = pixels.size()/2;
+        meio--;
+        if(pixels.size()%2==0){
+            mediana = (byte) ((pixels.get(meio)+pixels.get(meio+1))/2);
+        }else{
+            mediana = (byte) (pixels.get(meio));
+        }
+        return mediana;
+    }
+
     private void treatFrame(){
         this.currentFrame = taskBag.removeFirst();
         List<Byte> pixels = new ArrayList<>();
-        for (int i = 0; i< currentFrame.length; i++){
-            for(int j = 0; j< currentFrame[i].length; j++){
-                if(currentFrame[i-1] != null){
-                    pixels.add(currentFrame[i-1][j]);
-                    if(j-1>=0){
-                        pixels.add(currentFrame[i-1][j-1]);
-                        pixels.add(currentFrame[i][j-1]);
-                    }
-                    if(j+1<currentFrame[i].length){
-                        pixels.add(currentFrame[i-1][j+1]);
-                        pixels.add(currentFrame[i][j+1]);
-                    }
-                }
-                if(currentFrame[i+1] != null){
-                    pixels.add(currentFrame[i+1][j]);
-                    if(j-1>=0) pixels.add(currentFrame[i+1][j-1]);
-                    if(j+1<currentFrame[i].length) pixels.add(currentFrame[i-1][j+1]);
-                }
-                Collections.sort(pixels);
-                int meio = pixels.size()/2;
-                if(pixels.size()%2==0){
-                    int mediana = (pixels.get(meio)+pixels.get(meio+1))/2;
-                }
-
-                pixels.clear();
+        for (int i = 0; i< this.currentFrame.length; i++){
+            for(int j = 0; j< this.currentFrame[i].length; j++){
+                pixels = getNeighbours(this.currentFrame, i, j);
+                byte mediana = calcMediana(pixels);
+                if(this.currentFrame[i][j] < mediana-24 || this.currentFrame[i][j] > mediana+24) this.currentFrame[i][j] = mediana;
             }
         }
     }
