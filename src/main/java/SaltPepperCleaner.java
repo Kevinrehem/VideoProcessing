ce71125177ba7e7460bbeb8fe3e80c57126a2fae
KEVIN AVILA REHEM <0082327@IFMG.EDU.BR>
2025-05-30 20:49:09 -0300

    Commentary

--- src/main/java/SaltPepperCleaner.java
+++ src/main/java/SaltPepperCleaner.java
@@ -4,10 +4,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class SaltPepperCleaner extends Thread{
-    private static List<byte[][]> taskBag = new ArrayList<>();
-    private static List<byte[][]> fixedFrames = new ArrayList<>();
-    private byte[][] currentFrame;
+    private static List<byte[][]> taskBag = new ArrayList<>(); //De onde os cores vão pegar as tarefas
+    private static List<byte[][]> fixedFrames = new ArrayList<>(); //Destino final do frame corrigido
+    private byte[][] currentFrame; //o frame que será corrigido
+    private static Object key = new Object();
 
+    //Devolve uma List<> com todos os pixels vizinhos de um pixel cujo indice é passado como parametro
     private List<Byte> getNeighbours(byte[][] currentFrame, int i, int j){
         List<Byte> pixels = new ArrayList<>();
         if(currentFrame[i-1] != null){
@@ -31,6 +33,7 @@ public class SaltPepperCleaner extends Thread{
         return pixels;
     }
 
+    //Calcula a mediana baseado em uma lista de pixels
     private byte calcMediana(List<Byte> pixels){
         byte mediana;
         int meio = pixels.size()/2;
@@ -43,20 +46,22 @@ public class SaltPepperCleaner extends Thread{
         return mediana;
     }
 
+    //Método para tratar o frame atual, percorre a matriz de bytes e corrige os s
     private void treatFrame(){
         this.currentFrame = taskBag.removeFirst();
-        List<Byte> pixels = new ArrayList<>();
+        List<Byte> neighbours = new ArrayList<>();
         for (int i = 0; i< this.currentFrame.length; i++){
             for(int j = 0; j< this.currentFrame[i].length; j++){
-                pixels = getNeighbours(this.currentFrame, i, j);
-                byte mediana = calcMediana(pixels);
+                neighbours = getNeighbours(this.currentFrame, i, j);
+                byte mediana = calcMediana(neighbours);
                 if(this.currentFrame[i][j] < mediana-24 || this.currentFrame[i][j] > mediana+24) this.currentFrame[i][j] = mediana;
             }
         }
+        fixedFrames.add(this.currentFrame);
     }
 
 
-
+    //Carrega todos os frames para dentro de uma List
     public static void loadFrames(byte frames[][][]){
         for(int i=0; i< frames.length; i++){
             taskBag.add(frames[i]);
